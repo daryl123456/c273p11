6cfd19630bbd102e72ce5db5f582797582a05717
daryl13579 <19042545@myrp.edu.sg>
2021-01-26 14:46:58 +0800

    dfcwecwref

--- js/index.js
+++ js/index.js
@@ -4,15 +4,17 @@ $(document).ready(function () {
         console.log(JSON.parse(localStorage.getItem("array")))
         var array = localStorage.getItem("array");
         var arrays = JSON.parse(array);
-        var message = "<div class='card  text-white bg-dark'>";
+        var message = "";
+        
         for (var i = 0; i < arrays.length; i++) {
             var movie = arrays[i];
-
+            message += "<div class='card  text-white bg-dark'>"; 
             message += "<div class='card-header'>Title: " + movie.Title + "</div>";
             message += "<div class='card-body'><p class='card-text'>" + movie.Plot + "</p></div>";
+            message+="</div>";
 
         }
-        message+="</div>"
+        
         $("#contents").html(message);
     }
 });
